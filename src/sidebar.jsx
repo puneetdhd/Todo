f36c4aa81a923a43b202509cdfb6b9d97984e176
Puneet <puneetkumardhal@gmail.com>
2025-01-15 23:07:43 +0530

    2nd commit

--- src/sidebar.jsx
+++ src/sidebar.jsx
@@ -1,3 +1,18 @@
-export default function sidebar(){
+import DensityMediumIcon from '@mui/icons-material/DensityMedium';
+// import CategoriesSidebar from './sidebar';
+import { useState } from 'react';
 
+export default function sidebar(){
+    let [click,setClick]=useState('');
+    const triggerFn=()=>{
+        setClick((prev)=>!prev); //toggle State
+    }
+    return(
+        <>
+        <div className='container'>
+        <a onClick={triggerfn}><DensityMediumIcon/></a>
+
+        </div>
+        </>
+    )
 }
